68645abf535bac2375e964ea004ce10a0fbf4f76
ksusha <ksukrundysheva@mail.ru>
2025-10-05 15:50:52 +0300

    Delete sql/Фильтрация directory

--- sql/Фильтрация/02_filtering.sql
+++
@@ -1,9 +0,0 @@
-/* Запрос выбирает 10 последних доставленных заказов (deliver_order) курьера с courier_id = 100.
-Результат сортируется по времени выполнения в порядке убывания, чтобы сначала отображались самые свежие доставки.
-*/
-SELECT order_id,
-       time
-FROM   courier_actions
-WHERE  courier_id = 100
-   and action = 'deliver_order'
-ORDER BY time desc limit 10
